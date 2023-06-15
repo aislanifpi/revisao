0afb90904ec18e85bb0730d0f4d0e04f28e3777f
GabrielGomesdS <capic.2021118tads0297@aluno.ifpi.edu.br>
2023-06-15 11:48:50 -0300

    revisão atualizada

--- soma.py
+++ soma.py
@@ -2,8 +2,12 @@ def somar_numeros(numeros):
     resultado = 0
     for numero in numeros:
         resultado += numero
+    for numeros_d in outros_numeros:
+        resultado -= numeros_d
     return resultado
-
 numeros = [1, 2, 3, 4, 5]
+outros_numeros = [1, 3, 6]
 soma = somar_numeros(numeros)
 print(soma)
+
+

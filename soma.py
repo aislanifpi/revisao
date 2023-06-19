ff02a9304fe770e87173ca9283b937c2f01dd090
Marciely Ferreira <89710284+marcielysilva@users.noreply.github.com>
2023-06-19 15:17:44 -0300

    Revisão

--- soma.py
+++ soma.py
@@ -1,7 +1,11 @@
 def somar_numeros(numeros):
     resultado = 0
     for numero in numeros:
-        resultado += numero
+        if type(numero) == int or type(numero) == float:
+            resultado += numero
+        else:
+            print("Não é um número")
+            return False
     return resultado
 
 numeros = [1, 2, 3, 4, 5]
